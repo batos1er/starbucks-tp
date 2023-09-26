80556c037e070f91e32da3d4e479d509362bb666
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-09-26 15:04:45 +0200

    2.1

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-import { ProductsCategoryData } from "tp-kit/types"
+import { ProductData, ProductsCategoryData } from "tp-kit/types"
 
 export type ProductFiltersResult = {
   categoriesSlugs : string[],
@@ -16,4 +16,13 @@ export type NextPageProps<T = Record<string, string>> = {
    * e.g. : my-page?page=1&order=asc --> searchParams.page and searchParams.order
    */
   searchParams: { [key: string]: string | string[] | undefined }
+};
+
+export type ProductLineData = {
+  product : ProductData,
+  qty : number,
+};
+
+export type CartData = {
+  lines : ProductLineData[],
 };
