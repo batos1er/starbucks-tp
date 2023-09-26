78f57b4b6997ed4be8c1046776b69bfc323f9a03
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-09-26 14:56:36 +0200

    1.2

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
+import { ProductCartLine } from "../../../tp-kit/components/products/product-cart-line";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 export default function DevCartPage() {
@@ -23,6 +24,15 @@ export default function DevCartPage() {
 
       {/* Panier */}
       <section className="w-full lg:w-1/3 space-y-8">
+        {products.map((product) => (
+          <ProductCartLine key={product.id} product={product} qty={1} onQtyChange={console.log}>
+          </ProductCartLine>
+        ))}
+        <div className="flex justify-between">
+        <h3 className="w-full">Total</h3>
+        <div>10€</div>
+        </div>
+        <Button fullWidth type={"submit"}>Commander</Button>
 				
 				<Button variant={"outline"} fullWidth>Vider le panier</Button>
 			</section>
