d1bb6bda5227e1352e28545665268c21080f6ae8
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-09-27 11:53:01 +0200

    2.3 fini!!!!!!!!!!!!!

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -1,7 +1,85 @@
 import { create } from 'zustand'
+import { CartData, ProductLineData } from '../types'
+import { ProductData } from 'tp-kit/types'
 
-const useCart = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
+const useCart = create<CartData>((set) => ({
+  lines: [],
 }))
+
+// [...]
+
+/**
+ * Ajoute une nouvelle ligne au panier.
+ * Si le produit est déjà dans le panier, augmente la quantité de 1.
+ * 
+ * @param product 
+ */
+export function addLine(product: ProductData) {
+  useCart.setState((state) => {
+    const lines = [...state.lines];  
+    let ligneproduitId = lines.findIndex((line) => line.product.id === product.id)
+    if(ligneproduitId === -1){
+      lines.push({product : product,qty : 1});
+    }
+    else{
+      lines[ligneproduitId].qty = lines[ligneproduitId].qty +1;
+    }
+    return {lines:lines}
+  })
+}
+
+/**
+ * Modifie une ligne produit du panier
+ * 
+ * @param line 
+ */
+export function updateLine(line: ProductLineData) {
+  useCart.setState((state) =>{
+    const lines = [...state.lines];
+    let ligneId = lines.findIndex((ligne) => ligne.product.id === line.product.id)
+    lines[ligneId] = line;
+    return {lines:lines}
+  })
+}
+
+/**
+ * Supprime la ligne produit du panier 
+ * 
+ * @param productId 
+ * @returns 
+ */
+export function removeLine(productId: number) {
+  useCart.setState((state) =>{
+    const lines = [...state.lines];
+    let ligneId = lines.findIndex((ligne) => ligne.product.id === productId)
+    delete(lines[ligneId]);
+    return {lines:lines}
+  })
+}
+
+/**
+ * Vide le contenu du panier actuel
+ */
+export function clearCart() {
+  useCart.setState((state) =>{
+    return {lines : [],}
+  })
+}
+
+/**
+ * Calcule le total d'une ligne du panier
+ */
+export function computeLineSubTotal(line: ProductLineData): number {
+  return line.qty * line.product.price
+}
+
+/**
+ * Calcule le total du panier
+ */
+export function computeCartTotal(lines: ProductLineData[]): number {
+  let total = 0.00
+  for(let i = 0; i<lines.length; i++){
+    total += computeLineSubTotal(lines[i]);
+  }
+  return total
+}
